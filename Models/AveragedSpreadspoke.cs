809fe60a45d5482c4b42122658cae9e7d74fd09b
CJ Conti <cjc2818cjc@gmail.com>
2026-09-14 07:15:05 -0400

    Added new averaging model

--- Models/AveragedSpreadspoke.cs
+++ Models/AveragedSpreadspoke.cs
@@ -5,30 +5,18 @@ namespace NFLPredictionData.Models;
 /*
 schedule_date	schedule_season	schedule_week	schedule_playoff	team_home	score_home	score_away	team_away	team_favorite_id	spread_favorite	over_under_line	stadium	stadium_neutral	weather_temperature	weather_wind_mph	weather_humidity
 */
-public class Spreadspoke
+public class AveragedSpreadspoke
 {
-    public DateTime ScheduleDate { get; set; }
     public int ScheduleSeason { get; set; }
-    public int ScheduleWeek { get; set; }
-    public bool SchedulePlayoff { get; set; }
-    public string TeamHome { get; set; }
-
-    public int ScoreHome { get; set; }
-    public string TeamAway { get; set; }
-    public int ScoreAway { get; set; }
-
-    public string TeamFavorite { get; set;}
-
     public int SpreadFavorite { get; set;}
 
     public int SpreadActual { get; set;}
 
     public int SpreadDifference {get; set;}
 
-
     //Using int so I can average the number of games with SpreadCorrect by Year
     public int SpreadCorrect { get; set;}
-    public Spreadspoke()
+    public AveragedSpreadspoke()
     {
         
     }
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,4 +1,5 @@
 using MiniExcelLibs;
+using System.Collections; // Required namespace
 using System.Collections.Generic;
 using System.IO;
 
@@ -13,19 +14,75 @@ using NFLPredictionData.Models;
 
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.ObjectPool;
 
 public class IndexModel : PageModel
 {
+    public List<AveragedSpreadspoke> AverageBySeason(List<Spreadspoke> spreadspokes)
+    {
+        List<AveragedSpreadspoke> res=new List<AveragedSpreadspoke>();
+        spreadspokes= (List<Spreadspoke>) spreadspokes.OrderBy(s=>s.ScheduleSeason);
+
+        int currentSeason=spreadspokes[0].ScheduleSeason;
+        int count=0;
+        int spreadFavoriteSum=0;
+        int spreadActualSum=0;
+        int spreadDifference=0;
+        int spreadCorrect=0;
+
+        foreach (var spreadspoke in spreadspokes)
+        {
+            if(spreadspoke.ScheduleSeason!=currentSeason)
+            {
+                spreadFavoriteSum/=count;
+                spreadActualSum/=count;
+                spreadDifference/=count;
+                spreadCorrect/=count;
+                AveragedSpreadspoke averagedSpreadspoke=new AveragedSpreadspoke();
+
+                averagedSpreadspoke.ScheduleSeason=currentSeason;
+                averagedSpreadspoke.SpreadFavorite=spreadFavoriteSum/count;
+                averagedSpreadspoke.SpreadActual=spreadActualSum/count;
+                averagedSpreadspoke.SpreadDifference=spreadDifference/count;
+                averagedSpreadspoke.SpreadCorrect=spreadCorrect/count;
+                res.Add(averagedSpreadspoke);
+
+                currentSeason=spreadspoke.ScheduleSeason;
+
+                count=0;
+                spreadFavoriteSum=0;
+                spreadActualSum=0;
+                spreadDifference=0;
+                spreadCorrect=0;
+            }
+            count+=1;
+            spreadFavoriteSum+=spreadspoke.SpreadFavorite;
+            spreadActualSum+=spreadspoke.SpreadActual;
+            spreadDifference+=spreadspoke.SpreadDifference;
+            spreadCorrect+=spreadspoke.SpreadCorrect;
+        }
+
+        AveragedSpreadspoke averagedSpreadspoke=new AveragedSpreadspoke();
+        averagedSpreadspoke.ScheduleSeason=currentSeason;
+        averagedSpreadspoke.SpreadFavorite=spreadFavoriteSum/count;
+        averagedSpreadspoke.SpreadActual=spreadActualSum/count;
+        averagedSpreadspoke.SpreadDifference=spreadDifference/count;
+        averagedSpreadspoke.SpreadCorrect=spreadCorrect/count;
+        res.Add(averagedSpreadspoke);
+        return res;
+    }
     public void OnGet()
     {
         string filePath = "SpreadspokeData.xlsx";
 
         // This single line reads the sheet and binds it to your model list
-        IEnumerable<Spreadspoke> spreadkspokes = MiniExcel.Query<Spreadspoke>(filePath,sheetName:"Data");
+        IEnumerable<Spreadspoke> spreadspokesQuery = MiniExcel.Query<Spreadspoke>(filePath,sheetName:"Data");
+        List<Spreadspoke> spreadspokes=spreadspokesQuery.ToList();
 
-        foreach (var spreadkspoke in spreadkspokes)
+        List<AveragedSpreadspoke> averagedSpreadSpokes=AverageBySeason(spreadspokes);
+        foreach (var averagedSpreadspoke in averagedSpreadSpokes)
         {
-            Console.WriteLine($"{spreadkspoke.ScheduleDate} | {spreadkspoke.TeamHome} vs {spreadkspoke.TeamAway}");
+            Console.WriteLine($"{averagedSpreadspoke.ScheduleSeason} | {averagedSpreadspoke.SpreadDifference} and {averagedSpreadspoke.SpreadCorrect}");
         }
     }
 }
